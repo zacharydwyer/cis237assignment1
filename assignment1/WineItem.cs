e476ba8d026f9fc0b41df7f332170f5f65c80281
zacharydwyer <zdwyer4035@mail.kvcc.edu>
2015-09-17 14:42:27 -0400

    Created WineItem class and filled in most of its stuff.

---
+++ assignment1/WineItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    // Each instance of this class represents/mirrors one record in the WineList.csv file.
+    class WineItem
+    {
+        // All of these values are obtained from the WineList.csv file. They are all alphanumeric.
+        private string id, description, pack;                       
+
+        // Default constructor (required for assignment)
+        public WineItem()
+        {
+
+        }
+
+        // 3 parameter constructor - accepts all of WineItem's necessary attributes 
+        public WineItem(string id, string description, string pack)
+        {
+            // Give this instance of WineItem the given values.
+            this.id = id;
+            this.description = description;
+            this.pack = pack;
+        }
+
+        public string ToString()
+        {
+            
+        }
+    }
+}
